8e79c7cc4bad3eed47f73fb44db4e89abe40c8cc
Sofialv05 <sofalvsy@gmail.com>
2024-08-14 23:23:44 +0700

    fix: logo

--- app/src/config/appolloConnection.js
+++ app/src/config/appolloConnection.js
@@ -11,7 +11,7 @@ import { setContext } from "@apollo/client/link/context";
 import * as SecureStore from "expo-secure-store";
 
 const httpLink = createHttpLink({
-  uri: "https://b596-125-164-97-68.ngrok-free.app",
+  uri: "https://050catrip.sofalvsy-web.site/",
 });
 
 const authLink = setContext(async (_, { headers }) => {
